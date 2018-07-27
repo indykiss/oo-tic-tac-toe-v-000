17440f4aef7b99064ce43d514c95df6731f0c09c
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-27 17:28:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,8 +54,8 @@ class TicTacToe
   end
 
 
-  def current_player(board)
-    if turn_count(board) % 2 == 0
+  def current_player
+    if turn_count(@board) % 2 == 0
       return "X"
     else
       return "O"
